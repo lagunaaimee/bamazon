060ec55c45a1a35d5aaee6e7705e1a244bfadea5
Aimee <aimee.n.laguna@gmail.com>
2018-10-12 17:34:52 -0700

    installed npm modules

---
+++ bamazonCustomer.js
@@ -0,0 +1,36 @@
+var mysql = require("mysql");
+var inquirer = require("inquirer");
+
+// create the connection information for the sql database
+var connection = mysql.createConnection({
+    host: "localhost",
+  
+    // Your port; if not 3306
+    port: 3306,
+  
+    // Your username
+    user: "root",
+  
+    // Your password
+    password: "Eclipse03!",
+    database: "bamazon_db"
+  });
+  
+  // connect to the mysql server and sql database
+  connection.connect(function(err) {
+    if (err) throw err;
+    console.log("-Connected-")
+    // run the start function after the connection is made to prompt the user
+    start();
+  });
+
+  // function which prompts the user for what action they should take
+  function start() {
+    inquirer
+      .prompt({
+        name: "postOrBid",
+        type: "rawlist",
+        message: "Would you like to [POST] an auction or [BID] on an auction?",
+        choices: ["POST", "BID"]
+      }
+    )}
